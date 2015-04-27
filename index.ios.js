a9d98b36736e13f39d9d20ebc9a7614c58709ee0
Harrison Harnisch <hharnisc@gmail.com>
2015-04-26 17:12:06 -0700

    complete example

--- index.ios.js
+++ index.ios.js
@@ -7,46 +7,100 @@
 var React = require('react-native');
 var {
   AppRegistry,
+  Image,
+  ListView,
   StyleSheet,
   Text,
   View,
 } = React;
 
+var _ = require('lodash');
+var DDPClient = require("ddp-client");
+
 var todos = React.createClass({
+  getInitialState: function() {
+    return {
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => !_.isEqual(row1, row2),
+      }),
+      loaded: false,
+    };
+  },
+
+  componentDidMount: function() {
+    var that = this;
+    var ddpClient = new DDPClient({url: 'ws://localhost:3000/websocket'});
+
+    ddpClient.connect(() => ddpClient.subscribe('publicLists'));
+
+    // observe the lists collection
+    var observer = ddpClient.observe("lists");
+    observer.added = () => that.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
+    observer.changed = () => that.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
+    observer.removed = () => that.updateRows(_.cloneDeep(_.values(ddpClient.collections.lists)));
+  },
+
+  updateRows: function(rows) {
+    this.setState({
+     dataSource: this.state.dataSource.cloneWithRows(rows),
+     loaded: true,
+   });
+  },
+
   render: function() {
+    if (!this.state.loaded) {
+      return this.renderLoadingView();
+    }
+
+    return (
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderList}
+        style={styles.listView}
+      />
+    );
+  },
+
+  renderLoadingView: function() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+Control+Z for dev menu
+        <Text>
+          Loading lists...
         </Text>
       </View>
     );
-  }
+  },
+
+  renderList: function(list) {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.name}>{list.name}</Text>
+        <Text style={styles.incompleteCount}>{list.incompleteCount}</Text>
+      </View>
+    );
+  },
 });
 
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    flexDirection: 'row',
+    backgroundColor: 'white',
+    padding: 10,
   },
-  welcome: {
+  name: {
+    flex: 5,
+    fontSize: 18,
+  },
+  incompleteCount: {
+    flex: 1,
+    textAlign: 'center',
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    color: '#2196F3',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  listView: {
+    paddingTop: 20,
+    backgroundColor: 'white',
   },
 });
 
